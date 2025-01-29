d401c4d7b50b4c822d61e996a779eb5137285a24
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-01-29 12:51:13 -0500

    Updated routing on Account page: Signin/Signup/Prof

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,1 +1,33 @@
 import { Link } from "react-router-dom";
+import React from "react";
+
+export default function Signup() {
+  return (
+    <div id="wd-signup">
+      <h3>Sign Up</h3>
+      <input
+        type="text"
+        title="Enter Username"
+        placeholder="username"
+        className="wd-username"
+      />{" "}
+      <br />
+      <input
+        type="password"
+        title=""
+        placeholder="password"
+        className="wd-password"
+      />{" "}
+      <br />
+      <input
+        type="password"
+        title=""
+        placeholder="verify password"
+        className="wd-verify-password"
+      />{" "}
+      <br />
+      <Link to="Kambaz/Account/Signup">Sign up</Link> <br />
+      <Link to="Kambaz/Account/Signin">Sign in</Link>
+    </div>
+  );
+}
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,11 +1,18 @@
 import Account from "./Account";
 import { Route, Routes, Navigate } from "react-router";
 
+/**
+ * In the Kambaz screen, add the following routes so that the Account screen is the default.
+ * Confirm that the Signin screen is the landing page of the Kambaz Web application.
+ */
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
       <h1>Kambaz</h1>
-      <Account />
+      <Routes>
+        <Route path="/" element={<Navigate to="/Kambaz/Account/index" />} />
+        <Route path="/Account/" element={<Account />} />
+      </Routes>
     </div>
   );
 }
