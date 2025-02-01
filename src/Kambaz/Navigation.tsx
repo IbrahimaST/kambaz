65920302c41c2e47f04331e8773a9929981f58dc
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-02-01 15:49:58 -0500

    Added kambaz navigation component

--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import Account from "./Account";
 
 export default function KambazNavigation() {
-  return <div id="wd-kambaz-navigation"></div>;
+  return (
+    <div id="wd-kambaz-navigation">
+      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">
+        Northeastern
+      </a>
+      <Link to="/Kambaz/Account" id="wd-account-link">
+        Account
+      </Link>
+      <Link id="wd-dashboard-link" to="/Kambaz/Dashboard">
+        Dashboard
+      </Link>
+      <Link id="wd-courses-link" to="/Kambaz/Dashboard">
+        Courses
+      </Link>
+      <Link id="wd-calendar-link" to="/Kambaz/Calendar">
+        Calendar
+      </Link>
+      <Link id="wd-inbox-link" to="/Kambaz/Inbox">
+        Inbox
+      </Link>
+      <Link id="wd-labs-link" to="/Labs">
+        Labs
+      </Link>
+    </div>
+  );
 }
