6c364607e6246ed82f293d27c11236fdaec3028e
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-02-01 03:36:35 -0500

    started Dashboard component

---
+++ src/Kambaz/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Dashboard() {
+  return (
+    <div id="id-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+    </div>
+  );
+}
