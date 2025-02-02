2785ab5309bc3a4e79a105abc3e4c87951d47e22
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-02-02 16:48:25 -0500

    Fixed Modules routing

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,3 +1,4 @@
+import Modules from "./Modules";
 import CourseNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 
@@ -15,7 +16,7 @@ export default function Courses() {
             <Routes>
               <Route path="/" element={<Navigate to="Home" />}></Route>
               <Route path="/Home" element={<h2>Home</h2>} />
-              <Route path="Modules" element={<h2>Modules</h2>} />
+              <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<h2>Assignments</h2>} />
               <Route
                 path="Assignments/:aid"
