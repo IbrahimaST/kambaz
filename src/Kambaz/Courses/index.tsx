64b3e5e13e85fa0cfa66c2806a48c6efacff265c
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-02-02 17:43:48 -0500

    Replaced header with actual Home component

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,6 +1,7 @@
 import Modules from "./Modules";
 import CourseNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
+import Home from "./Home";
 
 export default function Courses() {
   return (
@@ -15,7 +16,7 @@ export default function Courses() {
           <td valign="top">
             <Routes>
               <Route path="/" element={<Navigate to="Home" />}></Route>
-              <Route path="Home" element={<h2>Home</h2>} />
+              <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<h2>Assignments</h2>} />
               <Route
