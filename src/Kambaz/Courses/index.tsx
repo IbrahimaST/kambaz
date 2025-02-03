acc556ef4db1bb6c2efa5bfe4b5f06b9d08461e5
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-02-02 19:53:54 -0500

    Added assignment editor route

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -3,6 +3,7 @@ import CourseNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Home from "./Home";
 import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/Editor";
 
 export default function Courses() {
   return (
@@ -20,6 +21,10 @@ export default function Courses() {
               <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<Assignments />} />
+              <Route
+                path="Assignments/:aid"
+                element={<AssignmentEditor />}
+              ></Route>
               <Route path="People" element={<h2>People</h2>} />
             </Routes>
           </td>
