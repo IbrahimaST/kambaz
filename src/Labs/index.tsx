1f6e91f21215015ee11f6ed5e67e7917c630b076
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-02-18 17:14:16 -0500

    Fixed error that tok me weeks -- Labs finally showing

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,21 +2,20 @@ import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import TOC from "./TOC";
-import { HashRouter, Navigate, Route, Routes } from "react-router";
+
+import { Navigate, Route, Routes } from "react-router";
 
 export default function Labs() {
   return (
-    <HashRouter>
-      <div>
-        <h1>Labs</h1>
-        <TOC />
-        <Routes>
-          <Route path="/" element={<Navigate to="Lab1" />}></Route>
-          <Route path="/Lab1" element={<Lab1 />}></Route>
-          <Route path="/Lab2/*" element={<Lab2 />}></Route>
-          <Route path="/Lab3" element={<Lab3 />}></Route>
-        </Routes>
-      </div>
-    </HashRouter>
+    <div>
+      <h1>Labs</h1>
+      <TOC />
+      <Routes>
+        <Route path="/" element={<Navigate to="Lab1" />}></Route>
+        <Route path="/Lab1" element={<Lab1 />}></Route>
+        <Route path="/Lab2/*" element={<Lab2 />}></Route>
+        <Route path="/Lab3" element={<Lab3 />}></Route>
+      </Routes>
+    </div>
   );
 }
