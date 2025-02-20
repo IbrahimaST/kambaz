2222c7aaca792ad4453c84fe1e8537943e35bfc5
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-02-19 20:42:25 -0500

    styled Assignments page

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,47 +1,118 @@
+import { Button, FormControl, ListGroup, Badge } from "react-bootstrap";
+import { BsPlus, BsGripVertical, BsThreeDots } from "react-icons/bs";
+import { LuClipboardPen } from "react-icons/lu";
+import { FaCheckCircle } from "react-icons/fa";
+
 export default function Assignments() {
   return (
-    <div id="wd-assignments">
-      <input placeholder="Search for Assignments" id="wd-search-assignment" />
-      <button id="wd-add-assignment-group">+ Group</button>
-      <button id="wd-add-assignment">+ Assignment</button>
-      <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button>+</button>{" "}
-      </h3>
-      <ul id="wd-assignment-list">
-        <li className="wd-assignment-list-item">
-          <a
-            href="#/Kambaz/Courses/1234/Assignments/123"
-            className="wd-assignment-link"
-          >
-            A1 - ENV + HTML
-          </a>{" "}
-          <br />
-          <p>
-            Multiple Modules | <b>Not available until</b> May 6 at 12:00am |{" "}
-            <b>Due</b> May 13 at 11:59pm | 100 pts
-          </p>
-        </li>
-        <li className="wd-assignment-list-item">
-          <a href="#/Kambaz/Courses/1234/Assignments/123">
-            A2 - CSS + BOOTSTRAP
-          </a>{" "}
-          <br />
-          <p>
-            Multiple Modules | <b>Not available until</b> May 13 at 12:00am |{" "}
-            <b>Due</b> May 20 at 11:59pm | 100 pts
-          </p>
-        </li>
-        <li className="wd-assignment-list-item">
-          <a href="#/Kambaz/Courses/1234/Assignments/123">
-            A3 - JAVASCRIPT + REACT
-          </a>{" "}
-          <br />
-          <p>
-            Multiple Modules | <b>Not available until</b> May 20 at 12:00am |{" "}
-            <b>Due</b> May 27 at 11:59pm | 100 pts
-          </p>
-        </li>
-      </ul>
+    <div id="wd-assignments" className="p-3">
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <FormControl
+          className="w-50"
+          placeholder="Search..."
+          id="wd-search-assignment"
+        />
+        <div>
+          <Button variant="secondary" className="me-2">
+            <BsPlus size={20} /> Group
+          </Button>
+          <Button variant="danger">
+            <BsPlus size={20} /> Assignment
+          </Button>
+        </div>
+      </div>
+
+      <ListGroup className="rounded-0" id="wd-assignments">
+        <ListGroup.Item className="p-0 fs-5" variant="secondary">
+          <div className="p-3 ps-2 bg-secondary d-flex align-items-center justify-content-between">
+            <div>
+              <BsGripVertical className="me-2 fs-3" /> ASSIGNMENTS
+            </div>
+            <div>
+              <Badge
+                bg="light"
+                text="dark"
+                className="px-3 py-2 rounded-pill d-flex align-items-center"
+              >
+                40% of Total <BsPlus />
+                <BsThreeDots />
+              </Badge>
+            </div>
+          </div>
+        </ListGroup.Item>
+
+        <ListGroup.Item className="p-0 border-start border-success border-3">
+          <div className="p-3 ps-2">
+            <div className="d-flex align-items-center">
+              <BsGripVertical size={25} className="me-2 fs-3" />
+              <LuClipboardPen size={25} className="text-success me-2" />
+              <a
+                href="#/Kambaz/Courses/1234/Assignments/123"
+                className="text-decoration-none text-dark fw-bold"
+              >
+                A1 - ENV + HTML
+              </a>
+              <FaCheckCircle size={25} className="text-success ms-auto" />
+              <BsGripVertical size={25} className="ms-2" />
+            </div>
+            <div className="ms-5">
+              <p className="mb-0">
+                <span className="text-danger">Multiple Modules</span> |
+                <b> Not available until</b> May 6 at 12:00am | <b>Due</b> May 13
+                at 11:59pm | 100 pts
+              </p>
+            </div>
+          </div>
+        </ListGroup.Item>
+
+        <ListGroup.Item className="p-0 border-start border-success border-3">
+          <div className="p-3 ps-2">
+            <div className="d-flex align-items-center">
+              <BsGripVertical size={25} className="me-2 fs-3" />
+              <LuClipboardPen size={25} className="text-success me-2" />
+              <a
+                href="#/Kambaz/Courses/1234/Assignments/123"
+                className="text-decoration-none text-dark fw-bold"
+              >
+                A2 - CSS + BOOTSTRAP
+              </a>
+              <FaCheckCircle size={25} className="text-success ms-auto" />
+              <BsGripVertical size={25} className="ms-2" />
+            </div>
+            <div className="ms-5">
+              <p className="mb-0">
+                <span className="text-danger">Multiple Modules</span> |
+                <b> Not available until</b> May 13 at 12:00am | <b>Due</b> May
+                20 at 11:59pm | 100 pts
+              </p>
+            </div>
+          </div>
+        </ListGroup.Item>
+
+        <ListGroup.Item className="p-0 border-start border-success border-3">
+          <div className="p-3 ps-2">
+            <div className="d-flex align-items-center">
+              <BsGripVertical size={25} className="me-2 fs-3" />
+              <LuClipboardPen size={25} className="text-success me-2" />
+              <a
+                href="#/Kambaz/Courses/1234/Assignments/123"
+                className="text-decoration-none text-dark fw-bold"
+              >
+                A3 - JAVASCRIPT + REACT
+              </a>
+              <FaCheckCircle size={25} className="text-success ms-auto" />
+              <BsGripVertical size={25} className="ms-2 " />
+            </div>
+            <div className="ms-5">
+              <p className="mb-0">
+                <span className="text-danger">Multiple Modules</span> |
+                <b> Not available until</b> May 20 at 12:00am | <b>Due</b> May
+                27 at 11:59pm | 100 pts
+              </p>
+            </div>
+          </div>
+        </ListGroup.Item>
+      </ListGroup>
     </div>
   );
 }
--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -2,6 +2,7 @@ import { ListGroup } from "react-bootstrap";
 import ModulesControls from "./ModulesControls";
 import LessonControlButtons from "./LessonControlButtons";
 import ModuleControlButtons from "./ModuleControlButtons";
+import { BsGripVertical } from "react-icons/bs";
 
 export default function Modules() {
   return (
@@ -14,31 +15,40 @@ export default function Modules() {
       <ListGroup className="rounded-0" id="wd-modules">
         <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary">
-            Week 1 <ModuleControlButtons />
+            <BsGripVertical className="me-2 fs-3" /> Week 1
+            <ModuleControlButtons />
           </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
               LEARNING OBJECTIVES
               <LessonControlButtons />
             </ListGroup.Item>
             <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
               Introduction to the course
               <LessonControlButtons />
             </ListGroup.Item>
             <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
               Learn what is Web Development
               <LessonControlButtons />
             </ListGroup.Item>
           </ListGroup>
         </ListGroup.Item>
         <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
+            Week 2
+          </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
               LESSON 1
               <LessonControlButtons />
             </ListGroup.Item>
             <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
               LESSON 2
               <LessonControlButtons />
             </ListGroup.Item>
