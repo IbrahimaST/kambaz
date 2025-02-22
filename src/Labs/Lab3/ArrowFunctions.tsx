4c71175ea5c869d9c8b76ec38093004e255d5380
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-02-21 20:42:50 -0500

    updated arrowfunctions

--- src/Labs/Lab3/ArrowFunctions.tsx
+++ src/Labs/Lab3/ArrowFunctions.tsx
@@ -1,17 +1,16 @@
-const subtract(a: number, b: number) => {
-    return a - b;
-}
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
 
 export default function ArrowFunctions() {
-    const threeMinusOne = 2;
-    console.log(subtract(3,1));
+  const threeMinusOne = 2;
+  console.log(subtract(3, 1));
 
-    return (
-        <div id="wd-arrow-functions">
-          <h4>New ES6 arrow functions</h4>
-          threeMinusOne = {threeMinusOne}   <br />
-          subtract(3, 1) = {subtract(3, 1)} <hr />
-        </div>
-      );
-    
+  return (
+    <div id="wd-arrow-functions">
+      <h4>New ES6 arrow functions</h4>
+      threeMinusOne = {threeMinusOne} <br />
+      subtract(3, 1) = {subtract(3, 1)} <hr />
+    </div>
+  );
 }
