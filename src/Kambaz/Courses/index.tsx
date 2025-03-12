2b08b0f741b34e4bd38dfe8259030992e6f73cb5
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-03-12 12:45:17 -0400

    implements breadcrumb component in course nav

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,6 +1,6 @@
 import Modules from "./Modules";
 import CourseNavigation from "./Navigation";
-import { Routes, Route, Navigate, useParams } from "react-router";
+import { Routes, Route, Navigate, useParams, useLocation } from "react-router";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
@@ -12,12 +12,13 @@ import { courses } from "../Database";
 export default function Courses() {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
 
   return (
     <div id="wd-courses" className="wd-main-content-offset">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        {course && course.name}
+        {course && course.name} &gt; {pathname.split("/")[4]}
       </h2>
       <hr />
       <div className="d-flex">
