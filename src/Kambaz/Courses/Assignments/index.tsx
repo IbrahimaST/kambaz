443f7c5bc7adbc074f00bdc5d7236b3690ba6aeb
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-03-25 20:20:57 -0400

    importing assignment into Database

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,9 +1,17 @@
+import { useParams } from "react-router-dom";
 import { Button, FormControl, ListGroup, Badge } from "react-bootstrap";
 import { BsPlus, BsGripVertical, BsThreeDots } from "react-icons/bs";
 import { LuClipboardPen } from "react-icons/lu";
 import { FaCheckCircle } from "react-icons/fa";
+import { assignments } from "../../Database";
 
 export default function Assignments() {
+  const { courseId } = useParams<{ courseId: string }>();
+
+  const courseAssignments = assignments.filter(
+    (assignment) => assignment.course === courseId
+  );
+
   return (
     <div id="wd-assignments" className="p-3">
       <div className="d-flex justify-content-between align-items-center mb-3">
@@ -41,77 +49,35 @@ export default function Assignments() {
           </div>
         </ListGroup.Item>
 
-        <ListGroup.Item className="p-0 border-start border-success border-3">
-          <div className="p-3 ps-2">
-            <div className="d-flex align-items-center">
-              <BsGripVertical size={25} className="me-2 fs-3" />
-              <LuClipboardPen size={25} className="text-success me-2" />
-              <a
-                href="#/Kambaz/Courses/1234/Assignments/123"
-                className="text-decoration-none text-dark fw-bold"
-              >
-                A1 - ENV + HTML
-              </a>
-              <FaCheckCircle size={25} className="text-success ms-auto" />
-              <BsGripVertical size={25} className="ms-2" />
+        {courseAssignments.map((assignment, index) => (
+          <ListGroup.Item
+            key={assignment._id}
+            className="p-0 border-start border-success border-3"
+          >
+            <div className="p-3 ps-2">
+              <div className="d-flex align-items-center">
+                <BsGripVertical size={25} className="me-2 fs-3" />
+                <LuClipboardPen size={25} className="text-success me-2" />
+                <a
+                  href={`#/Courses/${courseId}/Assignments/${assignment._id}`}
+                  className="text-decoration-none text-dark fw-bold"
+                >
+                  {assignment.title}
+                </a>
+                <FaCheckCircle size={25} className="text-success ms-auto" />
+                <BsGripVertical size={25} className="ms-2" />
+              </div>
+              <div className="ms-5">
+                <p className="mb-0">
+                  <span className="text-danger">Multiple Modules</span> |
+                  <b> Not available until</b> {` May ${6 + index * 7} `} at
+                  12:00am |<b> Due</b> {` May ${13 + index * 7} `} at 11:59pm |
+                  100 pts
+                </p>
+              </div>
             </div>
-            <div className="ms-5">
-              <p className="mb-0">
-                <span className="text-danger">Multiple Modules</span> |
-                <b> Not available until</b> May 6 at 12:00am | <b>Due</b> May 13
-                at 11:59pm | 100 pts
-              </p>
-            </div>
-          </div>
-        </ListGroup.Item>
-
-        <ListGroup.Item className="p-0 border-start border-success border-3">
-          <div className="p-3 ps-2">
-            <div className="d-flex align-items-center">
-              <BsGripVertical size={25} className="me-2 fs-3" />
-              <LuClipboardPen size={25} className="text-success me-2" />
-              <a
-                href="#/Kambaz/Courses/1234/Assignments/123"
-                className="text-decoration-none text-dark fw-bold"
-              >
-                A2 - CSS + BOOTSTRAP
-              </a>
-              <FaCheckCircle size={25} className="text-success ms-auto" />
-              <BsGripVertical size={25} className="ms-2" />
-            </div>
-            <div className="ms-5">
-              <p className="mb-0">
-                <span className="text-danger">Multiple Modules</span> |
-                <b> Not available until</b> May 13 at 12:00am | <b>Due</b> May
-                20 at 11:59pm | 100 pts
-              </p>
-            </div>
-          </div>
-        </ListGroup.Item>
-
-        <ListGroup.Item className="p-0 border-start border-success border-3">
-          <div className="p-3 ps-2">
-            <div className="d-flex align-items-center">
-              <BsGripVertical size={25} className="me-2 fs-3" />
-              <LuClipboardPen size={25} className="text-success me-2" />
-              <a
-                href="#/Kambaz/Courses/1234/Assignments/123"
-                className="text-decoration-none text-dark fw-bold"
-              >
-                A3 - JAVASCRIPT + REACT
-              </a>
-              <FaCheckCircle size={25} className="text-success ms-auto" />
-              <BsGripVertical size={25} className="ms-2 " />
-            </div>
-            <div className="ms-5">
-              <p className="mb-0">
-                <span className="text-danger">Multiple Modules</span> |
-                <b> Not available until</b> May 20 at 12:00am | <b>Due</b> May
-                27 at 11:59pm | 100 pts
-              </p>
-            </div>
-          </div>
-        </ListGroup.Item>
+          </ListGroup.Item>
+        ))}
       </ListGroup>
     </div>
   );
