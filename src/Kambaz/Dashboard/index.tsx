08b397004a794a752fbb95f7107e2352e2ef9c04
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-03-27 18:18:46 -0400

    added delete button to dashboard + added an add course feature

--- src/Kambaz/Dashboard/index.tsx
+++ src/Kambaz/Dashboard/index.tsx
@@ -8,18 +8,71 @@ import {
   Row,
   Col,
   Button,
+  FormControl,
 } from "react-bootstrap";
 import "../styles.css";
 import KambazNavigation from "../Navigation";
 import * as db from "../Database";
+import { useState } from "react";
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 export default function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState<any[]>(db.courses);
+
+  const [course, setCourse] = useState<any>({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "/images/latin.jpg",
+    description: "New Description",
+  });
+
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: uuidv4() };
+    setCourses([...courses, newCourse]);
+  };
+
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
   return (
     <div id="wd-dashboard" className="wd-main-content-offset">
       <KambazNavigation />
       <h1 id="wd-dashboard-title">Dashboard</h1>
       <hr />
+      <h5>
+        New Course
+        <button
+          id="wd-add-new-course-click"
+          className="btn btn-primary float-end"
+          onClick={addNewCourse}
+        >
+          Add
+        </button>
+      </h5>{" "}
+      <br />
+      <FormControl
+        value={course.name}
+        className="mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <FormControl
+        as="textarea"
+        value={course.description}
+        rows={3}
+        onChange={(e) => setCourse({ ...course, description: e.target.value })}
+      />
+      <br />
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
       <hr />
       <div id="wd-dashboard-courses">
@@ -33,7 +86,7 @@ export default function Dashboard() {
                 >
                   <CardImg
                     variant="top"
-                    src="/images/cooking.webp"
+                    src="public/images/psychology.jpg"
                     width={200}
                   />
                   <CardBody>
@@ -47,6 +100,16 @@ export default function Dashboard() {
                       {course.description}
                     </CardText>
                     <Button variant="primary">Go</Button>
+                    <button
+                      onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                      }}
+                      className="btn btn-danger float-end"
+                      id="wd-delete-course-click"
+                    >
+                      Delete
+                    </button>
                   </CardBody>
                 </Link>
               </Card>
