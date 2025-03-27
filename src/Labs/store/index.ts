c5219a4b328f953de1489701526d7ed6b5e6a326
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-03-27 16:29:44 -0400

    added counterReducer to the store

--- src/Labs/store/index.ts
+++ src/Labs/store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../Lab4/ReduxExamples/helloReducer";
+import counterReducer from "../Lab4/ReduxExamples/CounterRedux/counterReducer";
+
 
 const store = configureStore({
-    reducer: { helloReducer }
+    reducer: { helloReducer,
+        counterReducer
+    }
 })
 
 export default store;
