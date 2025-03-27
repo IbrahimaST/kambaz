1c922c6c5cf64bfa18b52c0c9bfa5c7c3816a8ce
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-03-27 17:09:20 -0400

    implemented todoList in redux for lab4

--- src/Labs/store/index.ts
+++ src/Labs/store/index.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../Lab4/ReduxExamples/helloReducer";
 import counterReducer from "../Lab4/ReduxExamples/CounterRedux/counterReducer";
-
+import addReducer from "../Lab4/ReduxExamples/AddRedux/addReducer";
+import todosReducer from "../Lab4/todos/todosReducer";
 
 const store = configureStore({
     reducer: { helloReducer,
-        counterReducer
+        counterReducer,
+        addReducer,
+        todosReducer
     }
 })
 
