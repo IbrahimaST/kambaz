b302416839ef97319b13a591fb7d2fef339686a4
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-04-12 14:34:36 -0400

    changed index

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -4,6 +4,9 @@ import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import KambazNavigation from "./Navigation";
 import "./styles.css";
+import * as db from "./Database";
+import { useState } from "react";
+import { v4 as uuidv4 };
 
 export default function Kambaz() {
   return (
@@ -13,7 +16,7 @@ export default function Kambaz() {
         <Routes>
           <Route path="/" element={<Navigate to="Account" />} />
           <Route path="/Account/*" element={<Account />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
+          // <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/Courses/:cid/*" element={<Courses />} />
           <Route path="/Calendar" element={<h1>Calendar</h1>} />
           <Route path="/Inbox" element={<h1>Inbox</h1>} />
