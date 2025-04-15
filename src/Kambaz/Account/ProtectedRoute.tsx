d987fa01e4354dfeb3b57264a2bb50fcaa087500
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-04-15 16:50:55 -0400

    working on protected route and user auth

--- src/Kambaz/Account/ProtectedRoute.tsx
+++ src/Kambaz/Account/ProtectedRoute.tsx
@@ -1,10 +1,27 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-export default function ProtectedRoute({ children }: { children: any }) {
+
+export default function ProtectedRoute({
+  children,
+  requireFaculty = false,
+}: {
+  children: any;
+  requireFaculty?: boolean;
+}) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  if (currentUser) {
-    return children;
-  } else {
+
+  if (!currentUser) {
     return <Navigate to="/Kambaz/Account/Signin" />;
   }
+
+  if (requireFaculty && currentUser.role !== "FACULTY") {
+    return children;
+  }
+
+  return children;
+}
+
+export function useCanEdit() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  return currentUser && currentUser.role === "FACULTY";
 }
--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -2,10 +2,12 @@ import { Routes, Route, Navigate } from "react-router";
 import Signin from "./Signin";
 import Profile from "./Profile";
 import Signup from "./Signup";
-import AccountNavigation from "./AccountNavigation";
+import AccountNavigation from "./Navigation";
 import "../styles.css";
+import { useSelector } from "react-redux";
 
 export default function Account() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
   return (
     <div id="wd-account-screen" className="wd-main-content-offset">
       <h2>Account</h2>
@@ -16,7 +18,18 @@ export default function Account() {
           </td>
           <td valign="top">
             <Routes>
-              <Route path="/" element={<Navigate to={"Signin"} />} />
+              <Route
+                path="/"
+                element={
+                  <Navigate
+                    to={
+                      currentUser
+                        ? "/Kambaz/Account/Profile"
+                        : "/Kambaz/Account/Signin"
+                    }
+                  />
+                }
+              />
               <Route path="/Signin" element={<Signin />} />
               <Route path="/Profile" element={<Profile />} />
               <Route path="/Signup" element={<Signup />} />
