5f91df12dd4babe88bb0e3b1191120006f589db0
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-04-15 15:21:41 -0400

    Added kambaz store

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import { HashRouter, Routes, Route, Navigate } from "react-router";
 import Kambaz from "./Kambaz";
 import Labs from "./Labs";
 import Courses from "./Kambaz/Courses";
+import { courses } from "./Kambaz/Database";
+import store from "./Kambaz/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <HashRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Navigate to="/Kambaz/Account/Signin" />} />
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kambaz/*" element={<Kambaz />} />
-          <Route path="/Courses/:cid/*" element={<Courses />} />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Routes>
+            <Route
+              path="/"
+              element={<Navigate to="/Kambaz/Account/Signin" />}
+            />
+            <Route path="/Labs/*" element={<Labs />} />
+            <Route path="/Kambaz/*" element={<Kambaz />} />
+            <Route
+              path="/Courses/:cid/*"
+              element={<Courses courses={courses} />}
+            />
+          </Routes>
+        </div>
+      </Provider>
     </HashRouter>
   );
 }
