eb6bb865e48a38ea1791187457a9dad437d96d13
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-04-15 16:52:52 -0400

    incorporated links array for user auth

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -4,26 +4,20 @@ import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
-        className="list-group-item active border border-0"
-        to="/Kambaz/Account/Signin"
-      >
-        Signin
-      </Link>{" "}
-      <Link
-        className="list-group-item text-danger border border-0"
-        to="/Kambaz/Account/Signup"
-      >
-        Signup
-      </Link>{" "}
-      <Link
-        className="list-group-item text-danger border border-0"
-        to="/Kambaz/Account/Profile"
-      >
-        Profile
-      </Link>
+      {links.map((link) => (
+        <Link
+          key={link}
+          className={`list-group-item ${
+            link === "Profile" ? "text-danger" : "active"
+          } border border-0`}
+          to={`/Kambaz/Account/${link}`}
+        >
+          {link}
+        </Link>
+      ))}
     </div>
   );
 }
