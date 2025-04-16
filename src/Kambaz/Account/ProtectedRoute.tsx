1e2d2d91a4e7b1c7c4a773fb069bf6e2055f490d
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-04-15 20:29:14 -0400

    changed ProtectedRoute

--- src/Kambaz/Account/ProtectedRoute.tsx
+++ src/Kambaz/Account/ProtectedRoute.tsx
@@ -1,27 +1,10 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({
-  children,
-  requireFaculty = false,
-}: {
-  children: any;
-  requireFaculty?: boolean;
-}) {
+export default function ProtectedRoute({ children }: { children: any }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-
-  if (!currentUser) {
+  if (currentUser) {
+    return children;
+  } else {
     return <Navigate to="/Kambaz/Account/Signin" />;
   }
-
-  if (requireFaculty && currentUser.role !== "FACULTY") {
-    return children;
-  }
-
-  return children;
-}
-
-export function useCanEdit() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  return currentUser && currentUser.role === "FACULTY";
 }
